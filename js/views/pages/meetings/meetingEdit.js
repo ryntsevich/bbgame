77f2f5220539f4410c46ab75e1d3dd2dd9467595
ryntsevich <ryntsevich.oksana@gmail.com>
2020-05-28 17:59:02 +0300

    refactor: page meetingEdit

--- js/views/pages/meetings/meetingEdit.js
+++ js/views/pages/meetings/meetingEdit.js
@@ -16,7 +16,6 @@ class MeetingEdit extends Component {
             this.meeting = meeting;
             this.modelUser.getUsersByIds(this.meeting.players).then(users => {
                 this.users = users;
-                // console.log(this.users)
                 resolve(meeting);
             });
         }
@@ -28,40 +27,39 @@ class MeetingEdit extends Component {
             let html;
 
             if (meeting) {
-                const { _id, gameName, day, time, place, description, players } = meeting;
+                const { gameName, day, time, place, description } = meeting;
 
                 html = `
                 <h1 class="page-title">Встреча</h1>
                 <div class="meet">
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Игра:</div>
-                    <div class="meet-propertis__content">${gameName}</div>
-                </div>
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Дата:</div>
-                    <input class="meet-propertis__content meet-edit-day" type="date" value="${day}">
-                </div>
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Время:</div>
-                    <input class="meet-propertis__content meet-edit-time" type="time" value="${time}">
-                </div>
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Место:</div>
-                    <input class="meet-propertis__content meet-edit-place" type="text" value="${place}">
-                </div>
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Участники:</div>
-                    ${this.users.map(user => `<div class="meet-propertis__content ">${user.username}</div>`).join('\n ')}
-                </div>
-                <div class="meet-propertis">
-                    <div class="meet-propertis__title">Описание:</div>
-                    <textarea class="meet-propertis__content meet-edit-description" type="text" value="${description}">${description}</textarea>
-                </div>
-                <div class="meet-propertis-btn">
-                    <button class="btn-save-meet">Сохранить правки</button>
-                    <a class="btn-cancel-meet" href ="#/meetings/${this.meeting._id}">Отмена</a>
-                </div>
-            </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Игра:</div>
+                        <div class="meet-propertis__content">${gameName}</div>
+                    </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Дата:</div>
+                        <input class="meet-propertis__content meet-edit-day" type="date" value="${day}">
+                    </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Время:</div>
+                        <input class="meet-propertis__content meet-edit-time" type="time" value="${time}">
+                    </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Место:</div>
+                        <input class="meet-propertis__content meet-edit-place" type="text" value="${place}">
+                    </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Участники:</div>
+                            ${this.users.map(user => `<div class="meet-propertis__content ">${user.username}</div>`).join('\n ')}
+                    </div>
+                    <div class="meet-propertis">
+                        <div class="meet-propertis__title">Описание:</div>
+                        <textarea class="meet-propertis__content meet-edit-description" type="text" value="${description}">${description}</textarea>
+                    </div>
+                    <div class="meet-propertis-btn">
+                        <button class="btn-save-meet">Сохранить правки</button>
+                        <a class="btn-cancel-meet" href ="#/meetings/${this.meeting._id}">Отмена</a>
+                    </div>
                 </div>
 `;
             } else {
@@ -73,7 +71,6 @@ class MeetingEdit extends Component {
 
     afterRender() {
         this.setActions();
-
     }
 
     setActions() {
@@ -83,15 +80,14 @@ class MeetingEdit extends Component {
             editPlace = document.getElementsByClassName('meet-edit-place')[0],
             editDescription = document.getElementsByClassName('meet-edit-description')[0];
 
-        btnSaveMeeting.addEventListener('click', () => this.editMeeting(editDay, editTime, editPlace, editDescription, btnSaveMeeting));
+        btnSaveMeeting.addEventListener('click', () => this.editMeeting(editDay, editTime, editPlace, editDescription));
     }
 
-    editMeeting(editDay, editTime, editPlace, editDescription, btnSaveMeeting) {
+    editMeeting(editDay, editTime, editPlace, editDescription) {
         this.meeting.day = editDay.value.trim();
         this.meeting.time = editTime.value.trim();
         this.meeting.place = editPlace.value.trim();
         this.meeting.description = editDescription.value.trim();
-
 
         this.modelMeeting.editMeeting(this.meeting).then(meeting => this.redirectToMeetingInfo(this.meeting._id));
     }
@@ -99,7 +95,6 @@ class MeetingEdit extends Component {
     redirectToMeetingInfo() {
         location.hash = `#/meetings/${this.meeting._id}`;
     }
-
 }
 
 export default MeetingEdit;
